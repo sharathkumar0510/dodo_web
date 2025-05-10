c5d91c133d82e1f4b771dd446de1b4174c6b7ee0
sharath <chenngonisharath@gmail.com>
2025-05-10 20:14:49 +0530

    Fix: Configure Next.js to allow placeholder images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    domains: ["via.placeholder.com", "placeholder.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "via.placeholder.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placeholder.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
